2fafec5e8ba432724463a235707da765e4c42872
Albert Lie <alberttri23@gmail.com>
2019-09-08 11:25:03 +0800

    add dynamic schema

--- server.js
+++ server.js
@@ -1,72 +1,52 @@
 var express = require('express');
 var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
+var { GraphQLObjectType, GraphQLSchema , GraphQLString } = require('graphql');
 
-// Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
-  input MessageInput {
-    content: String
-    author: String
-  }
-
-  type Message {
-    id: ID!
-    content: String
-    author: String
-  }
-
-  type Query {
-    getMessage(id: ID!): Message
-  }
-
-  type Mutation {
-    createMessage(input: MessageInput): Message
-    updateMessage(id: ID!, input: MessageInput): Message
-  }
-`);
-
-// If Message had any complex fields, we'd put them on this object.
-class Message {
-  constructor(id, {content, author}) {
-    this.id = id;
-    this.content = content;
-    this.author = author;
-  }
-}
-
-// Maps username to content
-var fakeDatabase = {};
-
-var root = {
-  getMessage: function ({id}) {
-    if (!fakeDatabase[id]) {
-      throw new Error('no message exists with id ' + id);
-    }
-    return new Message(id, fakeDatabase[id]);
+// Maps id to User object
+var fakeDatabase = {
+  'a': {
+    id: 'a',
+    name: 'alice',
   },
-  createMessage: function ({input}) {
-    // Create a random id for our "database".
-    var id = require('crypto').randomBytes(10).toString('hex');
-
-    fakeDatabase[id] = input;
-    return new Message(id, input);
-  },
-  updateMessage: function ({id, input}) {
-    if (!fakeDatabase[id]) {
-      throw new Error('no message exists with id ' + id);
-    }
-    // This replaces all old data, but some apps might want partial update.
-    fakeDatabase[id] = input;
-    return new Message(id, input);
+  'b': {
+    id: 'b',
+    name: 'bob',
   },
 };
+
+// Define the User type
+var userType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+  }
+});
+
+// Define the Query type
+var queryType = new GraphQLObjectType({
+  name: 'Query',
+  fields: {
+    user: {
+      type: userType,
+      // `args` describes the arguments that the `user` query accepts
+      args: {
+        id: { type: GraphQLString }
+      },
+      resolve: function (_, {id}) {
+        return fakeDatabase[id];
+      }
+    }
+  }
+});
+
+var schema = new GraphQLSchema({query: queryType});
 
 var app = express();
 app.use(
     '/graphql',
     graphqlHTTP({
         schema: schema,
-        rootValue: root,
         graphiql: true
     })
 );
